5bd9442391e7931c45a1bd183ade92dcf8e22a24
kraine <kraine212@gmail.com>
2022-11-24 16:57:08 +0200

    Block with timer

--- Source/src/js/modules/slider.js
+++ Source/src/js/modules/slider.js
@@ -15,11 +15,26 @@ export default class Slider {
             this.slideIndex = this.slides.length;
         }
 
+        try{
+            this.hanson.style.opacity = '0';
+            if (n === 3) {
+                this.hanson.classList.add('animated');
+                setTimeout(() => {
+                    this.hanson.style.opacity = '1';
+                    this.hanson.classList.add('slideInUp');
+                }, 3000);
+            } else {
+                this.hanson.classList.remove('slideInUp');
+            }
+        } catch(e) {}
+        
+
         this.slides.forEach(slide => {
             slide.style.display = 'none';
         });
 
         this.slides[this.slideIndex - 1].style.display = 'block';
+
     }
 
     plusSlides(n) {
@@ -28,6 +43,13 @@ export default class Slider {
     
 
     render() {
+
+        try {
+            this.hanson = document.querySelector('.hanson');
+        } catch(e) {}
+
+        
+
        this.btns.forEach(item => {
         item.addEventListener('click', () => {
             this.plusSlides(1);
