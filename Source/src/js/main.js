b25fdf291d1e6d04c7bc587b93b0997c97ec8675
kraine <kraine212@gmail.com>
2023-01-14 16:38:04 +0200

    finish sliders

--- Source/src/js/main.js
+++ Source/src/js/main.js
@@ -15,14 +15,20 @@ window.addEventListener('DOMContentLoaded', () => {
     const showUpSlider = new MiniSlider({
         container: '.showup__content-slider',   
         prev: '.showup__prev',
-        next: '.showup__next'
+        next: '.showup__next',
+        activeClass: 'card-active',
+        animate: true
+
     });
     showUpSlider.init();
 
         const modulesSlider = new MiniSlider({
         container: '.modules__content-slider',   
         prev: '.modules__info-btns .slick-prev',
-        next: '.modules__info-btns .slick-next'
+        next: '.modules__info-btns .slick-next',
+        activeClass: 'card-active',
+        animate: true,
+        autoplay: true
     });
 
     modulesSlider.init();
@@ -30,7 +36,8 @@ window.addEventListener('DOMContentLoaded', () => {
     const feedSlider = new MiniSlider({
         container: '.feed__slider',   
         prev: '.feed__slider .slick-prev',
-        next: 'feed__slider .slick-next'
+        next: '.feed__slider .slick-next',
+        activeClass: 'feed__item-active'
     });
 
     feedSlider.init();
--- Source/src/js/modules/slider/slider-mini.js
+++ Source/src/js/modules/slider/slider-mini.js
@@ -1,21 +1,43 @@
 import Slider from "./slider";
 
 export default class MiniSlider extends Slider{
-    constructor(container, next, prev){
-        super(container, next, prev);
+    constructor(container, next, prev, activeClass, animate, autoplay){
+        super(container, next, prev, activeClass, animate, autoplay);
     }
+
+    decorizeSlides() {
+        this.slides.forEach(slide => {
+            slide.classList.remove(this.activeClass);
+            if (this.animate) {
+                slide.querySelector('.card__title').style.opacity = '0.4';   
+                slide.querySelector('.card__controls-arrow').style.opacity = '0';   
+            }
+        });
+
+        this.slides[0].classList.add(this.activeClass);
+        if (this.animate) {
+            this.slides[0].querySelector('.card__title').style.opacity = '1';   
+            this.slides[0].querySelector('.card__controls-arrow').style.opacity = '1';   
+        }
+    }
+
+
+    nextSlide() {
+        this.container.appendChild(this.slides[0]);
+                this.decorizeSlides();
+    }
+
 
     bindTriggers() {
         if (this.next) {
-            this.next.addEventListener('click', () => {
-                this.container.appendChild(this.slides[0]);
-            });
+            this.next.addEventListener('click', () => this.nextSlide);
         }
 
         if (this.prev) {
             this.prev.addEventListener('click', () => {
                 let active = this.slides[this.slides.length - 1];
                 this.container.insertBefore(active, this.slides[0]);
+                this.decorizeSlides(); 
             });
         }
 
@@ -30,5 +52,10 @@ export default class MiniSlider extends Slider{
         `;
 
         this.bindTriggers();
+        this.decorizeSlides();
+
+        if(this.autoplay) {
+            setInterval(() => this.nextSlide(), 5000);
+        }
     }
 }
