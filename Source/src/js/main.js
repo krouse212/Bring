77b5bcae754045852302bda91e6eddf1f99b61b7
kraine <kraine212@gmail.com>
2023-01-18 16:20:12 +0200

    Forms

--- Source/src/js/main.js
+++ Source/src/js/main.js
@@ -2,6 +2,7 @@ import MainSlider from "./modules/slider/slider-main";
 import VideoPlayer from "./modules/playVideo";
 import MiniSlider from "./modules/slider/slider-mini";
 import Difference from "./modules/difference";
+import Form from "./modules/forms";
 
 window.addEventListener('DOMContentLoaded', () => {
 
@@ -45,4 +46,5 @@ window.addEventListener('DOMContentLoaded', () => {
 
     new Difference('.officerold', '.officernew', '.officer__card-item').init();
 
+    new Form('.form').init();
 });
--- Source/src/js/modules/forms.js
+++ Source/src/js/modules/forms.js
@@ -1,12 +1,80 @@
 export default class Form {
     constructor(forms){
         this.forms = document.querySelectorAll(forms);
+        this.inputs = document.querySelectorAll('input');
         this.message = {
             loading: 'Load...',
             success: 'Success!',
             failure: 'Fail'
         };
         this.path = 'assets/question.php';
+    }
+
+    clearInputs(){
+        this.inputs.forEach(item => {
+            item.value = '';
+        });
+    }
+
+    checkMailInputs() {
+        const mailInputs = document.querySelectorAll('[type="email"]');
+
+        mailInputs.forEach(input => {
+            input.addEventListener('keypress', function(e) {
+                if(e.key.match(/[^a-z 0-9 @ \.]/ig)) {
+                    e.preventDefault();
+                }
+            });
+        });
+    }
+
+    initMask() {
+
+        let setCursorPosition = (pos, elem) => {
+            elem.focus();
+    
+            if(elem.setSelectionRange) {
+                elem.setSelectionRange(pos, pos);
+            } else if (elem.createTextRange) {
+                let range = elem.createTextRange();
+    
+                range.collapse(true);
+                range.moveEnd('character', pos);
+                range.moveStart('character', pos);
+                range.select();
+            }
+        };
+    
+        function createMask(event) {
+            let matrix = '+1 (___) ___-____',
+                i = 0,
+                def = matrix.replace(/\D/g, ''),
+                val = this.value.replace(/\D/g, ''); 
+    
+            if (def.length >= val.length) {
+                val = def;
+            }
+    
+            this.value = matrix.replace(/./g, function(a) {
+                return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? '' : a;
+            });
+    
+            if (event.type === 'blur') {
+                if (this.value.length == 2) {
+                    this.value = '';
+                } else {
+                    setCursorPosition(this.value.length, this);
+                }
+            }
+        }
+    
+        let inputs = document.querySelectorAll('[name="phone"]');
+    
+        inputs.forEach(input => {
+            input.addEventListener('input', createMask);
+            input.addEventListener('focus', createMask);
+            input.addEventListener('blur', createMask);
+        });
     }
 
     async postData(url, data) {
@@ -19,6 +87,9 @@ export default class Form {
     }
 
     init(){
+        this.checkMailInputs();
+        this.initMask();
+
         this.forms.forEach(item => {
             item.addEventListener('submit', (e) => {
                 e.preventDefault();
@@ -41,6 +112,12 @@ export default class Form {
                     })
                     .catch(() => {
                         statusMessege.textContent = this.message.failure;
+                    })
+                    .finally(() => {
+                        this.clearInputs();
+                        setTimeout(() => {
+                            statusMessege.remove();
+                        }, 6000);
                     });
             });
         });
